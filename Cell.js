5cd3479e4202fc5e9825d623c7b2f86106eccb99
dreamgirlto <38843981+dreamgirlto@users.noreply.github.com>
2020-02-02 23:46:42 -0500

    Add files via upload

---
+++ Cell.js
@@ -0,0 +1,14 @@
+class Cell {
+
+    constructor(x, y, width, height) {
+        this.objects = [];
+        this.position = new Vector(x, y)
+        this.width = width;
+        this.height = height;
+    }
+
+    show(){
+        
+    }
+
+}
---
+++ Grid.js
@@ -0,0 +1,30 @@
+
+
+class Grid {
+
+    constructor(scene, width, height, x, y, cellSize){
+        this.scene = scene;
+        this.width = width;
+        this.height = height;
+        this.x = x;
+        this.y = y;
+        this.cells = [];
+        this.cellSize = cellSize;
+    }
+
+    init(){
+        let position 
+        while(position < this.bounds.width){
+            
+        }
+    }
+
+    insertObject(){
+
+    }
+
+    showGrid(){
+
+    }
+
+}
